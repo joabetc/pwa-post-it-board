63f48224071a75beed5b63bc08b5782151cacac2
Joabe Costa <joabetc@hotmail.com>
2018-01-20 19:36:59 -0200

    Saving images to Cache Storage

--- js/mural/Mural.js
+++ js/mural/Mural.js
@@ -21,6 +21,14 @@ const Mural = (function(_render, Filtro){
     }
 
     function preparaCartao(cartao) {
+        const urlImages = Cartao.pegaImagens(cartao)
+        urlImages.forEach(url => {
+            fetch(url).then(resposta => {
+                caches.open("ceep-imagens").then(cache => {
+                    cache.put(url, resposta)
+                })
+            })
+        })
         cartao.on("mudanca.**", salvaCartoes)
         cartao.on("remocao", ()=>{
             cartoes = cartoes.slice(0)
